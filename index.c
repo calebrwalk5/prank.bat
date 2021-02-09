066909ed8ce380988251b7bab18e529528dea389
Caleb W <51061101+calebrwalk5@users.noreply.github.com>
2021-02-09 14:58:32 -0500

    Create index.c

---
+++ index.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <Windows.h>
+
+int main() {
+  system ("start www.github.com/calebrwalk5");
+  printf("Calm down, bro. It's a joke. Chill.");
+  prank();
+  return 0;
+}
+
+int prank() {
+  // code goes here xd
+}
